f6e9b453e692b5c75f4fb741c962656bfd50c9ee
Ladislav Filip <ladislav.filip@gmail.com>
2023-04-16 21:29:23 +0200

    Init

---
+++ IdentityRazor/Program.cs
@@ -0,0 +1,66 @@
+
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+        };
+    });
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapGet("/api/load", async context =>
+    {
+        var result = new
+        {
+            msg = "Totot je odpoved"
+        };
+        var json = JsonSerializer.Serialize(result);
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync(json);
+    }).RequireAuthorization();
+});
+
+// app.MapRazorPages();
+
+app.Run();
---
+++ IdentityRazor/wwwroot/apiClient.js
@@ -0,0 +1,63 @@
+﻿const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.6NRiqq9P7KpR8Wrx2SWW6Xv4KMJkELoVBtoszBVcf1k"; // localStorage.getItem("jwtToken");
+
+async function fetchWithAuth(url, method = "GET", body = null) {
+
+    // Nastav hlavičky pro požadavek
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Authorization", `Bearer ${token}`);
+
+    // Nastav konfiguraci požadavku
+    const requestOptions = {
+        method,
+        headers,
+    };
+
+    if (body) {
+        requestOptions.body = JSON.stringify(body);
+    }
+
+    // Proveď požadavek a zpracuj odpověď
+    const response = await fetch(url, requestOptions);
+
+    if (response.ok) {
+        return await response.json();
+    } else {
+        throw new Error("Chyba při získání dat z API");
+    }
+}
+
+function decodeJwt(jwt) {
+    try {
+        // Rozdělení tokenu na části
+        const tokenParts = jwt.split(".");
+        if (tokenParts.length !== 3) {
+            throw new Error("Neplatný JWT token.");
+        }
+
+        // Dekódování zatížení z Base64Url na normální řetězec
+        const payloadBase64Url = tokenParts[1];
+        const payloadBase64 = payloadBase64Url.replace("-", "+").replace("_", "/");
+        const payloadJson = atob(payloadBase64);
+
+        // Převod řetězce JSON na objekt
+        const payload = JSON.parse(payloadJson);
+        return payload;
+    } catch (error) {
+        console.error("Chyba při dekódování JWT:", error);
+        return null;
+    }
+}
+
+const jwt = token;
+const decodedPayload = decodeJwt(jwt);
+
+console.log(decodedPayload);
+
+
+fetchWithAuth("/api/load").then((result) => {
+    console.log(result); // Vypíše "Hello" po 2 sekundách
+})
+    .catch((error) => {
+        console.error("Došlo k chybě:", error);
+    });
